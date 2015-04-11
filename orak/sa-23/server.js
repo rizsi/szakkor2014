d9bc25463e09e2219cece2609457b6feee78ba72
rizsi <andras.schmidt.hu@gmail.com>
2015-04-11 17:29:54 +0200

    Server implements message casing to all clients. Client id is also implemented.

--- orak/sa-23/server.js
+++ orak/sa-23/server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs=require('fs');
 var url=require('url');
 var nodeStatic=require('node-static');
+var util = require('util');
 var fileServer = new nodeStatic.Server('../../game');
 var program=fs.readFileSync('program.html');
 var elements=fs.readFileSync('elements.js');
@@ -19,29 +20,73 @@ var httpServer=http.createServer(function (req, res) {
 });
 httpServer.listen(9615);
 
+myLog=function(msg)
+{
+  console.log((new Date()) + msg);
+};
 // Based on: https://gist.github.com/martinsik/2031681
 var wsServer=new WebSocketServer({httpServer: httpServer});
+
+ClientsManager=function()
+{
+  this.currentId=0;
+  this.clients={};
+};
+ClientsManager.prototype.registerClient=function(client)
+{
+  client.id=this.currentId++;
+  this.clients[client.id]=client;
+  myLog("Client created: "+client.id);
+};
+ClientsManager.prototype.removeClient=function(client)
+{
+  delete(this.clients[client.id]);
+  myLog("Client disconnected: "+client.id);
+  this.sendAllButThis({id:-1},{disconnected: client.id});
+};
+ClientsManager.prototype.sendAllButThis=function(client, msg)
+{
+  for(id in this.clients)
+  {
+    if(id!=client.id)
+    {
+      this.clients[id].send(msg);
+    }
+  }
+};
+var clientsManager=new ClientsManager();
 
 ClientHandler=function(connection)
 {
   this.connection=connection;
-  connection.on('message', this.receive.bind(this));
+  clientsManager.registerClient(this);
+  this.send({id: this.id});
+//  console.log(util.inspect(connection, {showHidden: false, depth: null}));
+  connection.on('message', this.receiveRaw.bind(this));
   connection.on('close', this.close.bind(this));
 };
-ClientHandler.prototype.receive=function(message)
+ClientHandler.prototype.send=function(object)
+{
+  this.connection.sendUTF(JSON.stringify(object));
+};
+ClientHandler.prototype.receiveRaw=function(message)
 {
   if (message.type === 'utf8') {
     var msg=JSON.parse(message.utf8Data);
-    this.connection.sendUTF(JSON.stringify( {msg: 
-"Greetings from server!", rep: msg}));
+    this.receive(msg);
   }
+};
+ClientHandler.prototype.receive=function(msg)
+{
+  clientsManager.sendAllButThis(this, {from: this.id, msg: msg});
+//    this.send({msg: "Greetings from server!", rep: msg});
 };
 ClientHandler.prototype.close=function(connection)
 {
+  clientsManager.removeClient(this);
 };
 
 wsServer.on('request', function(request) {
-  console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
   var connection = request.accept(null, request.origin);
   var handler=new ClientHandler(connection);
 });
